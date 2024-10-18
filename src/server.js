e1c9844d0a52ffee7ef461465de7eabc418530db
lamnino08 <phanduclam02@gmail.com>
2024-10-18 15:09:00 +0700

    init strcuture

--- src/server.js
+++ src/server.js
@@ -2,26 +2,40 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./config/db");
+const path = require("path");
 
-// Import routes
+const dashboardRoutes = require("./routes/dashboardRoutes");
 const authRoutes = require("./routes/authRoutes");
 const reservationRoutes = require("./routes/reservationRoutes");
 const tableRoutes = require("./routes/tableRoutes");
 
 const app = express();
 
+// Set view engine
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use routes
+// Route for dashboard
+app.get("/dashboard", (req, res) => {
+  console.log("Here in the dashboard route");
+  res.render("dashboard", { title: "Dashboard" });
+});
+
+// Other routes
 app.use("/api/auth", authRoutes);
 app.use("/api/reservations", reservationRoutes);
 app.use("/api/tables", tableRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3003;
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
